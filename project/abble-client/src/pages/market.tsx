a00b259cdaa7af0e020d03b0244a1b07fc1e4fca
jin_itemscout <aosdb5640@naver.com>
2022-02-19 02:00:24 +0900

    market & joining room

--- project/abble-client/src/pages/market.tsx
+++ project/abble-client/src/pages/market.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './scss/market.scss'
 
 const MarketPage = () => {
   return (
-    <div>market</div>
+    <div>
+      <div className="market-header">
+        <div className="market-name">Market</div>
+        <button className='search-btn'>Search</button>
+        <button className='mint-btn'>Mint</button>
+      </div>
+      <div className="market-line">
+        <div className="nft-line">NFT 라인</div>
+        <div className="item-line">Item 라인</div>
+        <div className="tile-line">Tile 라인</div>
+      </div>
+    </div>
   )
 }
 
